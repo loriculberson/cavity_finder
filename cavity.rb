70ec4c3354637d0c59c855dc815a707caa214ee9
Lori Culberson <bigskybandit@sbcglobal.net>
2017-08-08 15:35:01 -0500

    Refactored cavity finder method

--- cavity.rb
+++ cavity.rb
@@ -4,26 +4,20 @@ class Cavity
     map_size = data[0].to_i
     grid = data[1..-1]
 
-    1.upto(map_size-2) do |r|
-      row = grid[r]
-      c = 1
-      while c < map_size-1
-        current = grid[r][c]
-        left    = grid[r][c-1]
-        right   = grid[r][c+1]
-        above   = grid[r-1][c]
-        below   = grid[r+1][c]
-
-        if is_cavity?(current, left, right, above, below)
-          grid[r][c] = 'X'
-        end
-        c+=1
+    1.upto(map_size-2) do |row|
+      1.upto(map_size-2) do |column|
+        grid[row][column] = 'X' if is_cavity?(grid, row, column)
       end
     end
     grid
   end
 
-  def is_cavity?(current, left, right, above, below)
+  def is_cavity?(grid, r,c)
+    current = grid[r][c]
+    left    = grid[r][c-1]
+    right   = grid[r][c+1]
+    above   = grid[r-1][c]
+    below   = grid[r+1][c]
     current > left && current > right && current > above && current > below
   end
 end
